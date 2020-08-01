8072cabd0b7015f858b058943877443acbf3ccfd
poldrack <poldrack@gmail.com>
2020-08-01 14:32:50 -0700

    add xfail

--- rtanalysis/test_3_type_fail.py
+++ rtanalysis/test_3_type_fail.py
@@ -3,10 +3,12 @@ test for rtanalysis
 - in this test, we will ensure that the function raises a ValueError
 """
 
+import pytest
 from rtanalysis.rtanalysis import RTAnalysis
 from rtanalysis.generate_testdata import generate_test_df
 
 
+@pytest.mark.xfail
 def test_dataframe_error():
     rta = RTAnalysis()
     test_df = generate_test_df(2, 1, 0.8)
