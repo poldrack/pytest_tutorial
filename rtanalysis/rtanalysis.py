431a13d934c3d8a3964079ea3c22b896a05e83bc
poldrack <poldrack@gmail.com>
2020-08-01 10:44:50 -0700

    add data generation function

--- rtanalysis/rtanalysis.py
+++ rtanalysis/rtanalysis.py
@@ -2,11 +2,13 @@
 - given a data frame with RT and accuracy, 
 compute mean RT for correct trials and mean accuracy
 """
-
+# %%
 import pandas as pd
 import numpy as np
+import scipy.stats 
 
 
+# %%
 class RTAnalysis:
     """[summary]
     """
@@ -36,7 +38,7 @@ class RTAnalysis:
         # ensure that RT's are non-negative
         assert rt.min() >= 0
         # ensure that accuracy values are boolean
-        assert len(set(acc.unique()).difference([ True, False])) == 0
+        assert len(set(accuracy.unique()).difference([True, False])) == 0
 
         if self.outlier_cutoff_sd is not None:
             cutoff = rt.std() * self.outlier_cutoff_sd
@@ -52,16 +54,49 @@ class RTAnalysis:
             print(f'mean RT: {self.meanrt_}')
             print(f'mean accuracy: {self.meanacc_}')
 
-def generate_test_df(meanRT, meanAcc, sdcutoff):
+
+# %%
+def generate_test_df(meanRT, sdRT, meanAcc, n=100):
     """
     generate simulated RT data for testing
 
     Args:
-        meanRT ([type]): [description]
-        meanAcc ([type]): [description]
-        sdcutoff ([type]): [description]
+        meanRT (float): mean RT (for correct trials)
+        meanAcc (float): mean accuracy (proportion, 0 <= meanAcc <= 1)
+        sdcutoff ([type]): outlier cutoff (default None for no cutoff)
     """
-
-
-if __name__ == "__main__":
     
+    rt = pd.Series(scipy.stats.weibull_min.rvs(2, loc=1, size=n))
+
+    # get random accuracy values and threshold for intended proportion
+    accuracy_continuous = np.random.rand(n) 
+    accuracy = pd.Series(
+        accuracy_continuous < scipy.stats.scoreatpercentile(
+            accuracy_continuous, 100 * meanAcc))
+
+    rt = rt.mask(~accuracy)
+    rt = scale_values(rt, meanRT, sdRT)
+    return(pd.DataFrame({'rt': rt, 'accuracy': accuracy}))
+
+
+def scale_values(values, mean, sd):
+    """scale values by given mean/sd
+
+    Args:
+        values (array-like): values to be scaled
+        mean (float): intended mean
+        sd (float): intended standard deviation
+    """
+    values = values * (sd / np.std(values))
+    values = (values - np.mean(values)) + mean
+
+    assert np.allclose(np.mean(values), mean)
+    assert np.allclose(np.std(values), sd)
+    return(values)
+
+
+# %%
+
+#if __name__ == "__main__":
+    
+
