b8e9304569eb949323a6df26ef9fc3e03cda39dc
poldrack <poldrack@gmail.com>
2020-08-01 13:51:23 -0700

    initial add

---
+++ rtanalysis/test_3_typefail.py
@@ -0,0 +1,13 @@
+"""
+test for rtanalysis
+- in this test, we will ensure that the function raises a ValueError
+"""
+
+from rtanalysis.rtanalysis import RTAnalysis
+from rtanalysis.generate_testdata import generate_test_df
+
+
+def test_dataframe_error():
+    rta = RTAnalysis()
+    test_df = generate_test_df(2, 1, 0.8)
+    rta.fit(test_df.rt, test_df.accuracy.loc[:10])
---
+++ rtanalysis/test_4_typesuccess.py
@@ -0,0 +1,15 @@
+"""
+test for rtanalysis
+- in this test, we will ensure that the function raises a ValueError
+"""
+
+import pytest
+from rtanalysis.rtanalysis import RTAnalysis
+from rtanalysis.generate_testdata import generate_test_df
+
+
+def test_dataframe_error_with_raises():
+    rta = RTAnalysis()
+    test_df = generate_test_df(2, 1, 0.8)
+    with pytest.raises(ValueError):
+        rta.fit(test_df.rt, test_df.accuracy.loc[:10])
