c5416335923839df97d2b6955a1e3d6cad3ddc2e
poldrack <poldrack@gmail.com>
2020-08-02 08:21:38 -0700

    change dataset indexing to fail regardless of data size

--- rtanalysis/test_3_type_fail.py
+++ rtanalysis/test_3_type_fail.py
@@ -15,4 +15,4 @@ from rtanalysis.generate_testdata import generate_test_df
 def test_dataframe_error():
     rta = RTAnalysis()
     test_df = generate_test_df(2, 1, 0.8)
-    rta.fit(test_df.rt, test_df.accuracy.loc[:10])
+    rta.fit(test_df.rt, test_df.accuracy.loc[1:])  # omit first datapoint
